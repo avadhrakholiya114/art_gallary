9e92cdc26df6f6e718c3baf4cf296f863b531c2a
avadh <rakholiyabalo@gmail.com>
2024-04-12 01:39:58 +0530

    user registration-login

--- art/urls.py
+++ art/urls.py
@@ -22,5 +22,6 @@ from . import views
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', views.home)
+                  path('', include('art_gallary.urls'))
+                 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- art_gallary/views.py
+++ art_gallary/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout
+from django.contrib.auth import authenticate
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# //artist 123
+def home(request):
+    return render(request, 'index.html')
+
+def  about(request):
+    return render(request, 'about.html')
+
+def register(request):
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2= request.POST.get('confirmPassword')
+        email = request.POST.get('email')
+        print(username, password, email)
+        my_user=User.objects.create_user(username, email, password)
+        my_user.save()
+        return redirect('login')
+    return render(request, 'register.html')
+
+def login_view(request):
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
